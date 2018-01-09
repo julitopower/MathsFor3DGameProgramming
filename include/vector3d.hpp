3766d9c85490d465090fb11c11527c67f2c285e9
Julio Delgado Mangas <julio.delgadomangas@gmail.com>
2018-01-09 03:12:35 +0000

    Improve comments

--- include/vector3d.hpp
+++ include/vector3d.hpp
@@ -12,23 +12,31 @@ namespace m3d {
     Vector3f(const std::vector<float>& data);
     Vector3f(const Vector3f& other);
 
+    // Add another vector
     void operator+=(const Vector3f& rhs);
     Vector3f operator+(const Vector3f& rhs) const;
 
+    // Multiply by a scalar
     void operator*=(float rhs);
     Vector3f operator*(float rhs) const;
 
+    // Divide by a scalar
     void operator/=(float rhs);
     Vector3f operator/(float rhs) const;
 
+    // Subtact a vector
     void operator-=(const Vector3f& rhs);
     Vector3f operator-(const Vector3f& rhs) const;
 
+    // Vector dot and cross products
     float dot(const Vector3f& rhs) const;
     Vector3f cross(const Vector3f& rhs) const;
 
+    // Access the data of the vector
     const float* data() const;
 
+    // Index operator for const and non-const
+    // vector references
     const float& operator[](int idx) const;
     float& operator[](int idx);
 
